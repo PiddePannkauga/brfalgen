bafba34347b5ac79bdc9c29727980ea266ed5441
Petter Mansson <petter.mansson1@gmail.com>
2022-02-20 18:12:57 +0100

    Context aware scrolling header

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,55 @@
 import type { NextComponentType } from "next";
 import header from "../styles/Header.module.css";
 import Link from "next/link";
+import cn from "classnames";
+import { useState } from "react";
+
+let previousScrollPosition = 0;
+
+const isScrollingDown = () => {
+  let currentScrolledPosition = window.scrollY || window.pageYOffset;
+  let scrollingDown;
+
+  if (currentScrolledPosition > previousScrollPosition) {
+    scrollingDown = true;
+  } else {
+    scrollingDown = false;
+  }
+  previousScrollPosition = currentScrolledPosition;
+
+  return scrollingDown;
+};
+
+let throttleWait = false;
+
+const throttle = (callback: any, time: number) => {
+  if (throttleWait) return;
+  throttleWait = true;
+  setTimeout(() => {
+    callback();
+    throttleWait = false;
+  }, time);
+};
 
 export const Header: NextComponentType = (props) => {
+  const [scrollingDown, setscrollingDown] = useState(false);
+
+  if (typeof window !== "undefined") {
+    const handleNavScroll = () => {
+      setscrollingDown(isScrollingDown());
+    };
+    window.addEventListener("scroll", () => {
+      throttle(handleNavScroll, 250);
+    });
+  }
+
+  const scrollUpStyle = cn({
+    [header.scrolldown]: scrollingDown,
+    [header.scrollup]: !scrollingDown,
+  });
+
   return (
-    <div className={header.header}>
+    <nav className={cn(header.header, scrollUpStyle)}>
       <Link href={"/"}>
         <h1>Välkommen till BRF Älgen</h1>
       </Link>
@@ -18,6 +63,6 @@ export const Header: NextComponentType = (props) => {
 
         <Link href={"/dokument"}>Dokument</Link>
       </div>
-    </div>
+    </nav>
   );
 };
